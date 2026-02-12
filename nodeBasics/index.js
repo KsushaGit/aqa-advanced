857ddd570bfe8df5ecd8e39b9881a5fd4a24151d
Oksana <ksusha_bor@ukr.net>
2026-02-12 23:36:45 +0000

    This is colourful text with a chalk library connected to the project

---
+++ nodeBasics/index.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello world in blue!'));
+//console.log(chalk.green("Green text"));
+console.log(chalk.red.bold("Hello" +" " , chalk.green.underline("Green Tree!")))
